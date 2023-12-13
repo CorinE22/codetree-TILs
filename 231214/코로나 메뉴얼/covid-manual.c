53de4a481f00b3db4c4ab0577fb930fff88921d7
CorinE22 <149644149+CorinE22@users.noreply.github.com>
2023-12-14 01:18:48 +0900

    Title: 코로나 메뉴얼, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231214/코로나 메뉴얼/covid-manual.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int a1, a2, a3;
+    char c1, c2, c3;
+
+    scanf("%c %d", &c1, &a1);
+    scanf("%c %d", &c2, &a2);
+    scanf("%c %d", &c3, &a3);
+
+    if(c1 = 'Y' && a1>=37)
+        {
+            if((c2 = 'Y' && a2 >= 37) || (c3 = 'Y' && a3 >= 37))
+                printf("E");
+            else
+                printf("N");
+        }
+    else
+    {
+        if((c2 = 'Y' && a2 >= 37) && (c3 = 'Y' && a3 >= 37))
+            printf("E");
+        else    
+            printf("N");
+    }
+    
+    return 0;
+}
