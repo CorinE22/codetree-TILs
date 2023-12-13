2594b119a944f986c59240a115123097e5720272
CorinE22 <149644149+CorinE22@users.noreply.github.com>
2023-12-13 23:24:23 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231213/두 사람/two-person.c
+++ 231213/두 사람/two-person.c
@@ -5,8 +5,8 @@ int main() {
     char b, d;
 
     // 두 사람의 정보를 입력받음
-    scanf("%d%c", &a, &b);
-    scanf("%d%c", &c, &d);
+    scanf("%d %c", &a, &b);
+    scanf("%d %c", &c, &d);
 
     // 두 조건 중 하나라도 만족하면 1을 출력
     printf("%d", (a >= 19 && b == 'M') || (c >= 19 && d == 'M'));
