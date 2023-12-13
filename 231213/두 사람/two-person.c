84c08bcf0b5ed45dc3559752dec7e0457813aa4d
CorinE22 <149644149+CorinE22@users.noreply.github.com>
2023-12-13 23:22:05 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231213/두 사람/two-person.c
+++ 231213/두 사람/two-person.c
@@ -4,8 +4,8 @@ int main() {
     int a, c;
     char b, d;
 
-    scanf("%d %c", &a, &b);
-    scanf("%d %c", &c, &d);
+    scanf("%d%c", &a, &b);
+    scanf("%d%c", &c, &d);
 
     printf("%d", (a>=19 || c>=19)&& (b == 'M' || d == 'M'));
 
